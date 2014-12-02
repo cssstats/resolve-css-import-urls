c1d8da220eac0ec585f90ca62f39a1b223221bff
John Otander <johnotander@gmail.com>
2014-12-01 21:28:36 -0700

    Parse imports into urls

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+'use strict';
+
 var getImports = require('get-imports');
+var getCssUrls = require('get-css-urls');
 var isUrl = require('is-url');
 
-module.exports = function(url) {
-  if(typeof url != 'string' || !isUrl(url)) {
-    console.log(url);
-    throw new TypeError('resolve-css-imports expected a url as a string');
+module.exports = function(url, css) {
+  if(typeof url != 'string' || typeof css != 'string' || !isUrl(url)) {
+    throw new TypeError('resolve-css-imports expected two string parameters (url, css)');
   }
+
+  var cssUrls = getImports(css).map(function(importStatement) {
+    return getCssUrls(importStatement)[0];
+  });
+
+  return cssUrls;
 }
--- test/test.js
+++ test/test.js
@@ -3,6 +3,12 @@ var resolveCssImports = require('..');
 
 describe('require-css-imports', function() {
 
+  it('should handle a standard import statement', function() {
+    assert.equal(
+      resolveCssImports('http://foo.com/my-css.css', '@import url("bar.css"); @import url("css/baz.css");'),
+      ['http://foo.com/bar.css', 'http://foo.com/baz.css']);
+  });
+
   it('should throw a type error if a string is not provided', function() {
     assert.throws(resolveCssImports);
   });
